b4195ab7e487cd5e481cb796d788a1d9e608834c
nathanlwebster <nathanlwebster@gmail.com>
2017-09-10 22:55:59 -0700

    Create url_for paths for edit and delete in template

--- project.py
+++ project.py
@@ -1,6 +1,6 @@
 
 # import CRUD Operations from Lesson 1
-from flask import Flask
+from flask import Flask, render_template, url_for
 from sqlalchemy import create_engine
 from database_setup import Base, Restaurant, MenuItem
 from sqlalchemy.orm import sessionmaker
@@ -18,16 +18,7 @@ session = DBSession()
 def restaurantMenu(restaurant_id):
     restaurant = session.query(Restaurant).filter_by(id = restaurant_id).one()
     items = session.query(MenuItem).filter_by(restaurant_id = restaurant.id)
-    output = ""
-    for i in items:
-        output += i.name
-        output += '<br>'
-        output += i.price
-        output += '<br>'
-        output += i.description
-        output += '<br>'
-        output += '<br>'
-    return output
+    return render_template('menu.html', restaurant = restaurant, items = items)
 
 # Task 1: Create route for newMenuItem function here
 
