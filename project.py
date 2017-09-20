98f47669445a3e67fde6941ae9305504c441779f
nathanlwebster <nathanlwebster@gmail.com>
2017-09-19 21:45:06 -0700

    Finish project.py

--- project.py
+++ project.py
@@ -22,7 +22,6 @@ def restaurantMenuJSON(restaurant_id):
 
 @app.route('/restaurants/<int:restaurant_id>/menu/<int:menu_id>/JSON')
 def menuItemJSON(restaurant_id, menu_id):
-    restaurant = session.query(Restaurant).filter_by(id = restaurant_id).one()
     item = session.query(MenuItem).filter_by(id = menu_id).one()
     return jsonify(MenuItem=[item.serialize])
 
